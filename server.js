ef7b1d8f4e61391bcf24fb6512f03bb2ef1ac6e4
joelreji <joelreji97@gmail.com>
2022-01-22 12:39:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ import path from 'path'
 
 const app = express()
 const __dirname = path.resolve();
+const PORT = process.env.PORT || '5000'
+
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "frontend", "build")))
+app.set("port", PORT)
 
 app.use("/api/v1/investments", investments)
 app.use("/api/v1/projects", projects)
